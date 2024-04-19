ff76283144d8f94b257f720dc1fc548c32e2da9f
Iury Gontijo Silva <iurygon.silva@gmail.com>
2024-04-18 23:52:39 -0300

    Add files via upload

--- Conceitos Básicos/Desafios/Desafios - Funções 4.py
+++ Conceitos Básicos/Desafios/Desafios - Funções 4.py
@@ -4,3 +4,16 @@ CRIE UMA FUNÇÃO QUE RECEBE DUAS LISTAS DE GOLS MARCADOS E SOFRIDOS POR UM TIME
 gols_marcados = [2, 1, 3, 1, 0]
 gols_sofridos = [1, 2, 2, 1, 3]
 
+def calcPontos(golsSof, golsMar):
+    pontos = 0
+    for partida in range(len(gols_marcados)):
+        if golsMar[partida] > golsSof[partida]:
+            pontos += 3
+        elif golsMar[partida] == golsSof[partida]:
+            pontos += 1
+        else:
+            continue
+    aproveitamento = (pontos * 100 ) / (3 * len(gols_marcados))
+    print(f'A pontuação do time ao final das partidas foi de {pontos}, um aproveitamento de {aproveitamento:.2f}%.')
+
+calcPontos(gols_sofridos, gols_marcados)
