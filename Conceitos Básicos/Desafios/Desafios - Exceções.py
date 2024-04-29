34b8feacb8f483e8959eb3da6d0c0f83cff0659d
Iury Gontijo Silva <iurygon.silva@gmail.com>
2024-04-28 23:59:35 -0300

    Add files via upload

--- Conceitos Básicos/Desafios/Desafios - Exceções.py
+++ Conceitos Básicos/Desafios/Desafios - Exceções.py
@@ -34,3 +34,33 @@ finally:
 
 #CRIE UM CÓDIGO QUE RECEBE UMA LISTA DE LISTAS COM AS NOTAS DE 3 ESTUDANTES EM UMA PROVA. CADA ESTUDANTE RESPONDEU 5 QUESTÕES E AS RESPOSTAS VARIAM DE 'A' ATÉ 'D'. CASO ALGUM VALOR
 #QUE NÃO CONDIZ COM AS RESPOSTAS ACEITAS FOR INSERIDO, SERÁ NECESSÁRIO LANÇAR UM ERRO COM A MENSAGEM 'A alternativa [alternativa] não é uma opção de alternativa válida'
+
+notasEst1 = list(input('Digite as notas do primeiro estudante:'))
+notasEst2 = list(input('Digite as notas do segundo estudante:'))
+notasEst3 = list(input('Digite as notas do terceiro estudante:'))
+resultados = [notasEst1, notasEst2, notasEst3]
+gabarito = ['D', 'A', 'B', 'C', 'A']
+
+def verificaErro(lista: list = []):
+    if len(lista) != 5:
+        raise IndexError ('O total de respostas preenchidas precisa ser cinco!')
+    for i in range(len(lista)):
+        if lista[i] not in ['A', 'B', 'C', 'D']:
+            raise ValueError(f'O valor {lista[i]} não é uma resposta válida!')
+
+def verificaAcertos(lista: list = [0]) -> int:
+    pontos = 0
+    comparativo = zip(lista, gabarito)
+    for resp in comparativo:
+        if resp[0] == resp[1]:
+            pontos += 1
+    print(f'O total de pontos desse aluno foi {pontos}.)
+
+verificaAcertos(notasEst1)
+for lista in range(len(resultados)): 
+    try:
+        verificaErro(resultados[lista])
+    except IndexError as IndE:
+        print(IndE, f'Erro na lista {lista + 1}, com os valores {resultados[lista]} ')
+    except ValueError as ValE:
+        print(ValE, f'Erro na lista {lista + 1}, com os valores {resultados[lista]} ')
