9dda3db070c3e4619479edded9a1af882ad05797
Iury Gontijo Silva <iurygon.silva@gmail.com>
2024-05-06 23:59:29 -0300

    Add files via upload

--- Conceitos Básicos/Desafios/Desafios - NumPy.py
+++ Conceitos Básicos/Desafios/Desafios - NumPy.py
@@ -3,3 +3,18 @@ import numpy as np
 
 url = 'https://raw.githubusercontent.com/allanspadini/numpy/dados/citrus.csv'
 dados = np.loadtxt(url, delimiter=',', usecols=np.arange(1,6,1), skiprows=1)
+
+
+#SELECIONAR APENAS O PESO E DIÂMETRO DAS LARANJAS E TORANJAS E GERAR GRÁFICAS DA RELAÇÃO ENTRE PESO E DIÂMETRO
+diamLaranja = dados[:5000, 0]
+pesoLaranja = dados[:5000, 1]
+diamToranja = dados[5000:, 0]
+pesoToranja  = dados[5000:, 1]
+
+import matplotlib.pyplot as plt
+
+# plt.plot(pesoLaranja, diamLaranja)
+# plt.show()
+
+plt.plot(pesoToranja, pesoToranja)
+plt.show()
